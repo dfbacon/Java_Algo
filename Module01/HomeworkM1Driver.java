ea513862085f2c3632e335887beea64557745257
Daniel Bacon <dbacon338@gmail.com>
2019-09-05 11:17:34 -0700

    Added working email validation method. Working as expected.

--- Module01/HomeworkM1Driver.java
+++ Module01/HomeworkM1Driver.java
@@ -22,10 +22,30 @@ public class HomeworkM1Driver {
 		}
 
 	}
-	
+
+
+    /**
+     * Checks if a given email is valid. A valid email must contain:
+     *  The @ symbol with at least one character before it.
+     *  A period (.).
+     *  Exactly three characters after the last period.
+     * @param email The string being evaluated.
+     * @return true if @email satisfies the given requirements; false otherwise.
+     */
 	public static boolean isValidEmail(String email) {
-		// YOUR CODE HERE
+
+	    //Smallest valid email string is 6 characters.
+	    if (email != null && email.length() >= 6) {
+
+	        //lastIndexOf() loops backward from the end of the string.
+            if (email.lastIndexOf('.', email.length()) == email.length() - 4) {
+
+                //indexOf() loops forward from index 1 to look for the @ symbol.
+                if (email.indexOf('@', 1) != -1)
+                    return true;
+            }
+        }
+
 		return false;
 	}
-
 }
