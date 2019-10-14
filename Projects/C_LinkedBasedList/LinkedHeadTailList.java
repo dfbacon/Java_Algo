7da8fa642b6266d5577b135505bb3ed7e625a37b
Daniel Bacon <dbacon338@gmail.com>
2019-10-14 11:59:37 -0700

    Added and tested all methods; working as expected.

--- Projects/C_LinkedBasedList/LinkedHeadTailList.java
+++ Projects/C_LinkedBasedList/LinkedHeadTailList.java
@@ -1,40 +1,236 @@
-/**
- * Use the same nested Node class that is used in LList. I have provided a shell file for you.
- *
- * Note: You must use a head and tail reference! For full credit, do not add any additional instance data variables. In particular, do not add a counter variable! (The purpose of this project is to get more practice with  nodes, not keeping count!)
- *
- * For full credit, implement an efficient solution when possible. All of the methods in the class should be O(1) or O(n). O(n2) methods will not receive full credit.
- *
- * Your class must compile and have the following implemented methods. Follow the API descriptions from the interface file and the additional characteristics listed below. You can choose whether or not to include a constructor.
- *
- * (12 points) addFront(T)
- * (12 points) addBack(T)
- * (12 points) removeFront()
- * (12 points) removeBack()
- * (6 points) contains(T)
- * (6 points) indexOf(T)
- * (6 points) lastIndexOf(T)
- * (6 points) getEntry(int)
- * (4 points) display()
- * see the tester program for the format of the output
- * the output should list the contents plus the value of head and tail (when the list is not empty)
- * (2 points) clear()
- * (2 points) isEmpty()
- * (5 points) size()
- * (15 points) Style and Efficiency
- * Use a head and tail reference.
- * Do not use additional instance data variables other than head and tail.
- * Ensure that methods are O(1) or O(n).
- * Your class should follow Java coding conventions and best practices.
- * Follow naming conventions for variables, classes, and methods.
- * Reduce duplicated code.
- */
-
 public class LinkedHeadTailList<T> implements HeadTailListInterface<T> {
 
 	private Node head, tail;
-   
-   // YOUR CLASS HERE!
+
+
+	public LinkedHeadTailList() {
+		head = null;
+		tail = null;
+	}
+
+
+
+	public void addFront(T newEntry) {
+		Node newNode = new Node(newEntry);
+
+		if (isEmpty()) {
+			head = newNode;
+			tail = head;
+		}
+		else {
+			newNode.setNextNode(head);
+			head = newNode;
+		}
+	}
+
+
+	public void addBack(T newEntry) {
+		Node newNode = new Node(newEntry);
+
+		if (isEmpty()) {
+			head = newNode;
+			tail = head;
+		}
+		else {
+			tail.setNextNode(newNode);
+			tail = newNode;
+		}
+	}
+
+
+	public T removeFront() {
+		Node tempNode = head;
+
+		if (!isEmpty()) {
+			if (size() > 1) {
+				head = head.getNextNode();
+				tempNode.setNextNode(null);
+			}
+			else {
+				head = null;
+				tail = null;
+			}
+
+			return tempNode.getData();
+		}
+
+		return null;
+	}
+
+
+	public T removeBack() {
+		Node tempNode = head;
+
+		if (!isEmpty()) {
+			if (size() > 1) {
+				while (tempNode.getNextNode() != tail) {
+					tempNode = tempNode.getNextNode();
+				}
+
+				tail = tempNode;
+				tempNode = tempNode.getNextNode();
+				tail.setNextNode(null);
+			}
+			else {
+				head = null;
+				tail = null;
+			}
+
+			return tempNode.getData();
+		}
+
+		return null;
+	}
+
+
+	public void clear() {
+		if (!isEmpty()) {
+			Node tempNode = head;
+
+			while (tempNode.getNextNode() != null) {
+				head = head.getNextNode();
+
+				tempNode.setData(null);
+				tempNode.setNextNode(null);
+				tempNode = head;
+			}
+
+			tempNode.setData(null);
+			tempNode = null;
+			head = null;
+			tail = null;
+		}
+	}
+
+
+	public T getEntry(int givenPosition) {
+		Node target = head;
+
+		if (givenPosition < 0) {
+			return null;
+		}
+
+		for (int index = 0; index < givenPosition; index++) {
+			if (target.getNextNode() != null) {
+				target = target.getNextNode();
+			}
+			else {
+				return null;
+			}
+		}
+
+		return target.getData();
+	}
+
+
+
+	public void display() {
+		Node currentNode = head;
+
+		System.out.print("[");
+
+		while (currentNode != null) {
+			System.out.print(currentNode.getData());
+
+			if (currentNode.getNextNode() != null) {
+				System.out.print(", ");
+			}
+
+			currentNode = currentNode.getNextNode();
+		}
+
+		System.out.print("]");
+
+		if (!isEmpty()) {
+			System.out.println("\thead=" + head.getData() + " tail=" + tail.getData());
+		}
+	}
+
+
+	public int indexOf(T anEntry) {
+		Node currentNode = head;
+		int index = 0;
+
+		while (currentNode != null) {
+			if (currentNode.getData().equals(anEntry)) {
+				return index;
+			}
+
+			index++;
+			currentNode = currentNode.getNextNode();
+		}
+
+		return -1;
+	}
+
+
+	public int lastIndexOf(T anEntry) {
+		Node currentNode = head;
+		int index = 0;
+		int firstFound = -1, lastFound = -1;
+
+
+		while (currentNode != null) {
+			if (currentNode.getData().equals(anEntry)) {
+				if (firstFound == -1) {
+					firstFound = index;
+				}
+				else {
+					lastFound = index;
+				}
+			}
+
+			index++;
+			currentNode = currentNode.getNextNode();
+		}
+
+		if (firstFound != -1) {
+			if (lastFound > firstFound) {
+				return lastFound;
+			}
+		}
+
+		return firstFound;
+	}
+
+
+	public boolean contains(T anEntry) {
+		Node currentNode = head;
+
+		if (!isEmpty()) {
+			while (currentNode != null) {
+				if (currentNode.getData().equals(anEntry)) {
+					return true;
+				}
+
+				currentNode = currentNode.getNextNode();
+			}
+		}
+
+		return false;
+	}
+
+
+	public int size() {
+		Node tempNode = head;
+		int count = 0;
+
+		while (tempNode != null) {
+			count++;
+			tempNode = tempNode.getNextNode();
+		}
+
+		return count;
+	}
+
+
+	public boolean isEmpty() {
+		if (head == null) {
+			return true;
+		}
+
+		return false;
+	}
 
 
 	private class Node {
