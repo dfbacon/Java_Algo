5a558bf239902fe033bddc8828bf6810ec473e54
Daniel Bacon <dbacon338@gmail.com>
2019-11-06 16:29:50 -0800

    Working with indentations.

--- Module10/HomeworkM10FolderDriver.java
+++ Module10/HomeworkM10FolderDriver.java
@@ -6,46 +6,46 @@ public class HomeworkM10FolderDriver {
 	public static void main(String[] args) {
     	Folder folder1 = new Folder("Folder1");
     	Folder folder2 = new Folder("Folder2");
-//    	Folder folder3 = new Folder("Folder3");
-//    	Folder folder4 = new Folder("Folder4");
+    	Folder folder3 = new Folder("Folder3");
+    	Folder folder4 = new Folder("Folder4");
     	Folder folder1a = new Folder("Folder1a");
     	Folder folder1b = new Folder("Folder1b");
-//    	Folder folder2a = new Folder("Folder2a");
-//    	Folder folder2b = new Folder("Folder2b");
-//    	Folder folder2c = new Folder("Folder2c");
-//    	Folder folder4a = new Folder("Folder4a");
-//    	Folder folder4b = new Folder("Folder4b");
+    	Folder folder2a = new Folder("Folder2a");
+    	Folder folder2b = new Folder("Folder2b");
+    	Folder folder2c = new Folder("Folder2c");
+    	Folder folder4a = new Folder("Folder4a");
+    	Folder folder4b = new Folder("Folder4b");
     	folder1.addFolder(folder1a);
     	folder1.addFile(new File("File1-1"));
     	folder1.addFile(new File("File1-2"));
     	folder1.addFolder(folder1b);
-//    	folder2.addFolder(folder2a);
-//    	folder2.addFolder(folder2b);
-//    	folder2.addFolder(folder2c);
-//    	folder3.addFile(new File("File3-1"));
-//    	folder4.addFolder(folder4a);
-//    	folder4.addFile(new File("File4-1"));
-//    	folder4.addFile(new File("File4-2"));
-//    	folder4.addFolder(folder4b);
+    	folder2.addFolder(folder2a);
+    	folder2.addFolder(folder2b);
+    	folder2.addFolder(folder2c);
+    	folder3.addFile(new File("File3-1"));
+    	folder4.addFolder(folder4a);
+    	folder4.addFile(new File("File4-1"));
+    	folder4.addFile(new File("File4-2"));
+    	folder4.addFolder(folder4b);
 
     	folder1a.addFile(new File("File1a-1"));
     	folder1a.addFile(new File("File1a-2"));
     	folder1b.addFile(new File("File1b-1"));
-//    	folder2a.addFile(new File("File2a-1"));
-//    	folder2a.addFile(new File("File2a-2"));
-//    	folder2a.addFile(new File("File2a-3"));
-//    	folder2c.addFile(new File("File2c-1"));
-//    	folder4b.addFile(new File("File4b-1"));
-//    	folder4b.addFile(new File("File4b-2"));
-//    	folder4b.addFile(new File("File4b-3"));
+    	folder2a.addFile(new File("File2a-1"));
+    	folder2a.addFile(new File("File2a-2"));
+    	folder2a.addFile(new File("File2a-3"));
+    	folder2c.addFile(new File("File2c-1"));
+    	folder4b.addFile(new File("File4b-1"));
+    	folder4b.addFile(new File("File4b-2"));
+    	folder4b.addFile(new File("File4b-3"));
     	
     	Folder topFolder = new Folder("TopFolder");
     	topFolder.addFolder(folder1);
     	topFolder.addFolder(folder2);
-//    	topFolder.addFolder(folder3);
-//    	topFolder.addFolder(folder4);
+    	topFolder.addFolder(folder3);
+    	topFolder.addFolder(folder4);
 
-//    	printFolderContentsRecursive(topFolder);
+    	printFolderContentsRecursive(topFolder);
     	printFolderContentsWithStack(topFolder);
 
 	}
@@ -111,72 +111,45 @@ public class HomeworkM10FolderDriver {
 
 	private static int folderIndentHelper(Folder top, Folder targetFolder) {
 
-		// current issue, top isn't coming in correct every time.
-
-		//TESTING
-//		System.out.println("\n***** START HELPER ******\n target is: " + targetFolder + "\ntop is: " + top.getFolderList());
-
-
 		int indentLevel = 0;
+		Stack<Folder> folderStack = new Stack<Folder>();
+		Stack<Folder> subFolderStack = new Stack<Folder>();
 
 		if (!targetFolder.equals(top)) {
 
-			Stack<Folder> subFolderStack = new Stack<Folder>();
 			List<Folder> folders = top.getFolderList();
 			List<Folder> subFolders;
-
 			indentLevel = 1;
 
-			// TESTING
-//			System.out.println("\n***** BEFORE HELPER WHILE LOOP ******\ntop is: " + top.getFolderList());
+			for (Folder folder : folders) {
 
-//			System.out.println("############# in helper ###########");
-//			System.out.println("\ttop is: " + top.getFolderList());
+				folderStack.push(folder);
+			}
 
-			while (!folders.isEmpty()) {
+			while (!folderStack.isEmpty()) {
 
-				// TESTING
-//				System.out.println("\n***** TOP HELPER WHILE LOOP ******\n\ttop is: " + top.getFolderList());
+				if (targetFolder.equals(folderStack.peek())) {
 
-				// iterate over all folders in a level
-				for(int index = 0; index < folders.size(); index++) {
+					return indentLevel;
+				} else {
 
-					if (targetFolder.equals(folders.get(index))) {
+					subFolders = folderStack.pop().getFolderList();
 
+					for (Folder subFolder : subFolders) {
 
-						// before this point top has been altered..
-						//TESTING
-//						System.out.println("\n***** RETURNING FROM HELPER ******\n\ttop is: " + top.getFolderList());
-
-
-						return indentLevel;
+						subFolderStack.push(subFolder);
 					}
-
-					else {
-
-						subFolders = folders.get(index).getFolderList();
-
-						for (Folder subFolder : subFolders) {
-
-							subFolderStack.push(subFolder);
-						}
-					}
-
-					// TESTING
-//					System.out.println("\n***** END HELPER WHILE LOOP ******\n\ttop is: " + top.getFolderList());
 				}
 
-				folders.clear(); // reset folders for next level
+				if (folderStack.isEmpty()) {
 
-				// TESTING
-//				System.out.println("\n***** AFTER CLEAR ******\n\ttop is: " + top.getFolderList());
+					while (!subFolderStack.isEmpty()) {
 
-				while (!subFolderStack.isEmpty()) {
+						folderStack.push(subFolderStack.pop());
+					}
 
-					folders.add(subFolderStack.pop());
+					indentLevel++;
 				}
-
-				indentLevel++;
 			}
 		}
 
